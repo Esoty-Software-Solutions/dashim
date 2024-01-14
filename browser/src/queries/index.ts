9136cee62c747334ad456ae48bc57bfc38a9bea4
Abdulhamid <zoubi.abdulhamid@gmail.com>
2024-01-14 19:42:59 +0200

    fix: partially patch ts errors on client

--- browser/src/queries/index.ts
+++ browser/src/queries/index.ts
@@ -3,15 +3,11 @@ import { httpBatchLink } from "@trpc/client/links/httpBatchLink";
 
 import type { AppRouter } from "dashim_server/src/routers/_.router";
 
+/// <reference types="../../../server/node_modules/.prisma/client" />
+
 const url = import.meta.env.VITE_FULL_TRPC_URL;
 
+// @ts-ignore
 export const client = createTRPCProxyClient<AppRouter>({
   links: [httpBatchLink({ url })],
-});
-
-const s = client.user.findManyUser.query({});
-
-const t = client.user.createManyUser.mutate({});
-const a = client.user.createOneUser.mutate({
-  data: { gender: { connectOrCreate: {} } },
 });
