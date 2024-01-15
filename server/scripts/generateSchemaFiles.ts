46b7a9209f203a40e8aba1f9454f8c6d7f2524b4
Abdul Fourteia <raoph40@gmail.com>
2024-01-15 19:23:31 +0200

    completed schema generation script

--- server/scripts/generateSchemaFiles.ts
+++ server/scripts/generateSchemaFiles.ts
@@ -2,23 +2,100 @@ import { Prisma } from "@prisma/client";
 import fs from "fs";
 import path from "path";
 
+const TableNames = Object.values(Prisma.ModelName);
 const __dirname = path.dirname(new URL(import.meta.url).pathname.substring(1));
 
-const TableNames = Object.values(Prisma.ModelName);
-
 const templatePath = path.join(__dirname, "modelFileTemplate.ts");
-
 const template = fs.readFileSync(templatePath, "utf-8");
 
-const outputDirectory: string = path.resolve(__dirname, "..", "src", "models");
+const sourceEnumsDir: string = path.resolve(
+  __dirname,
+  "..",
+  "prisma",
+  "generated",
+  "schemas",
+  "enums",
+);
 
-TableNames.forEach((TableName) => {
-  const tableName = TableName.charAt(0).toLowerCase() + TableName.slice(1);
-  const output: string = template
-    .replace(/User/g, TableName)
-    .replace(/user(?!Id)/g, tableName);
+const outputEnumsDir: string = path.resolve(
+  __dirname,
+  "..",
+  "src",
+  "schemas",
+  "enums",
+);
 
-  fs.writeFileSync(path.join(outputDirectory, `${tableName}.model.ts`), output);
-});
+const sourceObjectDir: string = path.resolve(
+  __dirname,
+  "..",
+  "prisma",
+  "generated",
+  "schemas",
+  "objects",
+);
 
-console.log("output is: ", outputDirectory);
+const outputObjectDir: string = path.resolve(
+  __dirname,
+  "..",
+  "src",
+  "schemas",
+  "objects",
+);
+
+// console.info("source are:\n", sourceEnumsDir, "\n", sourceObjectDir);
+// console.info("output are:\n", outputEnumsDir, "\n", outputObjectDir);
+
+// Remove all files in the output directory
+console.info("removing files in output directory");
+fs.rmSync(outputEnumsDir, { recursive: true, force: true });
+fs.rmSync(outputObjectDir, { recursive: true, force: true });
+// Recreate the directories
+fs.mkdirSync(outputEnumsDir, { recursive: true });
+fs.mkdirSync(outputObjectDir, { recursive: true });
+
+// Read the source directory
+function copyFiles(sourceDirectory: string, destinationDirectory: string) {
+  console.info(
+    "copying files from: ",
+    sourceDirectory,
+    "to: ",
+    destinationDirectory,
+  );
+  let fileCounter = 0;
+  fs.readdir(sourceDirectory, (err, files) => {
+    if (err) {
+      console.error("Could not list the directory.", err);
+      process.exit(1);
+    }
+
+    // Map each file to a promise that resolves when the file is copied
+    const copyPromises = files.map((file) => {
+      return new Promise<void>((resolve, reject) => {
+        const sourceFilePath = path.join(sourceDirectory, file);
+        const destinationFilePath = path.join(destinationDirectory, file);
+
+        fs.copyFile(sourceFilePath, destinationFilePath, (err) => {
+          if (err) {
+            console.error(`File ${file} could not be copied.`, err);
+            reject(err);
+          } else {
+            fileCounter++;
+            resolve();
+          }
+        });
+      });
+    });
+
+    // Wait for all the files to finish copying
+    Promise.all(copyPromises)
+      .then(() => {
+        console.info(`copied ${fileCounter} files`);
+      })
+      .catch((err) => {
+        console.error("An error occurred while copying the files.", err);
+      });
+  });
+}
+
+copyFiles(sourceEnumsDir, outputEnumsDir);
+copyFiles(sourceObjectDir, outputObjectDir);
