90a031d30dd513df35e6aa17b2c2130cf3104b08
Abdul Fourteia <raoph40@gmail.com>
2024-01-15 17:37:59 +0200

    starting on the autogen scripts

---
+++ server/scripts/schemaObjectFileTemplate.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+import type { Prisma } from "@prisma/client";
+
+// UserCreateManyInputObjectSchema
+// UserUncheckedCreateInputObjectSchema
+// UserWhereInputObjectSchema
+// UserOrderByWithRelationInputObjectSchema
+// UserWhereUniqueInputObjectSchema
+// UserScalarFieldEnumSchema
+// UserOrderByWithAggregationInputObjectSchema
+// UserScalarWhereWithAggregatesInputObjectSchema
+// UserUpdateManyMutationInputObjectSchema
+// UserUncheckedUpdateInputObjectSchema
+// UserCountAggregateInputObjectSchema
+// UserMinAggregateInputObjectSchema
+// UserMaxAggregateInputObjectSchema
