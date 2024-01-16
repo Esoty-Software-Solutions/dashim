5eb5e83aef104e55e3556ef6deaf3c805db5393c
Abdul Fourteia <raoph40@gmail.com>
2024-01-16 11:19:43 +0200

    commeted out the seeder to fix it in another branch

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -1,65 +1,65 @@
-import { PrismaClient } from '@prisma/client'
-import bcrypt from 'bcryptjs'
-import { User } from '@models/user'
-import { create } from 'domain'
-import { createId } from '@paralleldrive/cuid2'
-import { SeedHelper } from '../scripts/generateSeederFiles'
-import * as functions from '../prisma/fakeData/functions'
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcryptjs";
+import { User } from "@models/user";
+import { create } from "domain";
+import { createId } from "@paralleldrive/cuid2";
+import { SeedHelper } from "../scripts/generateSeederFiles";
+import * as functions from "../prisma/fakeData/functions";
 
-const prisma = new PrismaClient()
+// const prisma = new PrismaClient()
 
-async function seed() {
-  const email = 'a@a.com'
+// async function seed() {
+//   const email = 'a@a.com'
 
-  // cleanup the existing database
-  await prisma.user.deleteMany().catch(() => {
-    // no worries if it doesn't exist yet
-  })
+//   // cleanup the existing database
+//   await prisma.user.deleteMany().catch(() => {
+//     // no worries if it doesn't exist yet
+//   })
 
-  const hashedPassword = await bcrypt.hash('12345678', 10)
+//   const hashedPassword = await bcrypt.hash('12345678', 10)
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const user = await User.createOne({
-    data: {
-      email,
-      passwordHash: hashedPassword,
-      username: 'aaaa',
-      birthDate: new Date(),
-      avatar: 'sdfasdfasd',
-      firstName: 'aaaa',
-      lastName: 'aaaa',
-      id: createId(),
-      searchName: 'aaaa',
-      DeviceToken: {
-        create: {
-          name: 'fds',
-          id: createId(),
-          deviceType: {
-            create: {
-              name: 'ios',
-              id: createId(),
-            },
-          },
-        },
-      },
-    },
-  })
+//   // eslint-disable-next-line @typescript-eslint/no-unused-vars
+//   const user = await User.createOne({
+//     data: {
+//       email,
+//       passwordHash: hashedPassword,
+//       username: 'aaaa',
+//       birthDate: new Date(),
+//       avatar: 'sdfasdfasd',
+//       firstName: 'aaaa',
+//       lastName: 'aaaa',
+//       id: createId(),
+//       searchName: 'aaaa',
+//       DeviceToken: {
+//         create: {
+//           name: 'fds',
+//           id: createId(),
+//           deviceType: {
+//             create: {
+//               name: 'ios',
+//               id: createId(),
+//             },
+//           },
+//         },
+//       },
+//     },
+//   })
 
-  // await User.createOne({
-  //   data: {
-  //     email: email2,
-  //     passwordHash: hashedPassword,
-  //   },
-  // })
+//   // await User.createOne({
+//   //   data: {
+//   //     email: email2,
+//   //     passwordHash: hashedPassword,
+//   //   },
+//   // })
 
-  console.log(`Database has been seeded. 🌱`)
-}
+//   console.log(`Database has been seeded. 🌱`)
+// }
 
-seed()
-  .catch((e) => {
-    console.error(e)
-    process.exit(1)
-  })
-  .finally(async () => {
-    await prisma.$disconnect()
-  })
+// seed()
+//   .catch((e) => {
+//     console.error(e)
+//     process.exit(1)
+//   })
+//   .finally(async () => {
+//     await prisma.$disconnect()
+//   })
