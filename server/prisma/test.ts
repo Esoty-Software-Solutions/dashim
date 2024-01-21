bdded7693235278f0b363c9c514f6956d3d9e2a9
AbdulRaoph Fourteia (Abdul) <12877426+afourteia@users.noreply.github.com>
2024-01-21 11:04:55 +0200

    Feat/refactor db seeder (#11)
    * typo fixes
    
    * normalize subscriber institution relationship
    
    * make TenantMembers singular
    
    * disable some fakeData prisma settings
    
    * created a test file for db auto seeder
    
    * auto seeding script can seed all tables
    
    * some fixes
    
    * db changes
    
    * updated DeviceToken table. added token field and updated the indexes
    
    * combo of name + policyId should be unique
    
    * combo of name + policyId should be unique
    
    * unique city names for each country
    
    * added specialized names based on tables
    
    * added scalable random seeder
    
    * added new enum data for some tables

---
+++ server/prisma/test.ts
@@ -0,0 +1,234 @@
+import * as functions from "../prisma/fakeData/functions";
+import { SeedHelper, type Object } from "../scripts/generateSeederFiles";
+import { unGuardedPrisma } from "@config/db";
+import { type Prisma, PrismaClient } from "@prisma/client";
+import * as enumData from "./enumData";
+
+// const obj1 = functions.fakeBeneficiary();
+// console.log(Object.keys(obj1));
+// console.log(Object.keys(obj1).length);
+
+// const obj2 = functions.fakeBeneficiaryComplete();
+// console.log(Object.keys(obj2));
+// console.log(Object.keys(obj2).length);
+
+// const obj1Keys = Object.keys(obj1);
+// const obj2Keys = Object.keys(obj2);
+
+// const difference = obj2Keys.filter((key) => !obj1Keys.includes(key));
+
+// console.log("differences: \n", difference);
+
+// console.log("testing undefined");
+
+// console.log("thirdName ", obj2["thirdName"] === undefined);
+// console.log("firstName ", obj2["firstName"] === undefined);
+// console.log("aaa ", obj2["aaa"] === undefined);
+// console.log("thirdName ", "thirdName" in obj2);
+// console.log("firstName ", "firstName" in obj2);
+// console.log("aaa ", "aaa" in obj2);
+
+// console.log(Object.keys(SeedHelper.functions));
+// const a = SeedHelper.functions["fakeBeneficiary"];
+// console.log(a);
+// console.log(a());
+
+// console.log("SortedTables############\n", SeedHelper.sortedTable);
+// console.log("tableDep############\n", SeedHelper.tableDep);
+// console.log("tableRefs###########\n", SeedHelper.tableRefs);
+
+// const tableRowIds: { [key: string]: string;  } = {};
+type TableRowIds = { [key: string]: { id: string }[] };
+const tableRowIds: TableRowIds = {};
+// const tableRowIds: { [key: string]: string[]; id?: string[] } = {};
+
+export type enumTableType = {
+  [K in keyof typeof enumData]: object[];
+} & {
+  [key: string]: object[];
+};
+
+const enumTableData: enumTableType = enumData;
+
+const SKIP_SEEDED_TABLES = true;
+const prisma = new PrismaClient();
+
+await prisma.$connect();
+console.info("prisma connected...");
+
+console.info("Seeding ", SeedHelper.tablesFullList.length, " tables");
+// TODO: script to seed tables that have no dependencies
+for (const TableName of SeedHelper.tablesFullList) {
+  const tableName = TableName.charAt(0).toLowerCase() + TableName.slice(1);
+  if (!(TableName in SeedHelper.tableDep)) {
+    console.log(TableName, ":", "no dependencies");
+    // const test = await unGuardedPrisma['country'].deleteMany({where: {}});
+    // const test = await unGuardedPrisma['country'].createMany({data: {}, skipDuplicates: true});
+    // const test = await unGuardedPrisma["country"].count({ where: {} });
+    // const test = await unGuardedPrisma['country'].findMany({where: {}, take: 0, skip: 0, orderBy:{}, select: {}});
+    const count: number = await (unGuardedPrisma as any)[tableName].count(); // TODO: put in a try and catch
+    console.log("Count before insertion: ", count);
+    if (count > 0 && SKIP_SEEDED_TABLES) {
+      // await (unGuardedPrisma as any)[tableName].deleteMany();
+      const size = 1000; // replace with your desired size
+      const dataArray = await Promise.all(
+        new Array(size).fill(null).map(async (data: Object) => {
+          // console.log("starting an iteration");
+          data = SeedHelper.functions["fake" + TableName + "Complete"]();
+          await SeedHelper.fieldOverride(data);
+          // console.log("ending an iteration");
+          Object.keys(data).forEach((key) => {
+            if (Number.isInteger(data[key])) {
+              if (data[key] > 2147483647 || data[key] < -2147483648) {
+                // console.error(
+                //   `Value ${data[key]} at key ${key} is too large for an INT4 field.`,
+                // );
+                data[key] = Math.floor(Math.random() * 2147483647);
+                // console.info(`${key} is reassigned to ${data[key]}.`);
+              }
+            }
+            if (Array.isArray(data[key])) {
+              data[key] = data[key].map((value: number) => {
+                if (Number.isInteger(value)) {
+                  if (value > 2147483647 || value < -2147483648) {
+                    // console.error(
+                    //   `Value ${value} at key ${key} array is too large for an INT4 field.`,
+                    // );
+                    value = Math.floor(Math.random() * 2147483647);
+                    // console.info(`Element of ${key} is reassigned to ${value}.`);
+                  }
+                }
+                return value;
+              });
+            }
+          });
+          return data;
+        }),
+      );
+      console.log("pushing data for ", TableName);
+      const CreateResults = await (unGuardedPrisma as any)[
+        tableName
+      ].createMany({
+        data: dataArray,
+        skipDuplicates: true,
+      });
+      console.log(
+        "Added ",
+        CreateResults.count,
+        " rows to ",
+        TableName,
+        " table",
+      );
+      tableRowIds[TableName] = await (unGuardedPrisma as any)[
+        tableName
+      ].findMany({
+        select: { id: true },
+      });
+    }
+  }
+}
+
+// console.log("tableRowIds############\n", tableRowIds);
+
+// TODO: script to seed tables that have dependencies
+for (const TableName of SeedHelper.sortedTable) {
+  const tableName = TableName.charAt(0).toLowerCase() + TableName.slice(1);
+  if (TableName in SeedHelper.tableDep) {
+    console.log(TableName, ":", SeedHelper.tableDep[TableName]);
+    let isTableReady = true; // are all dependencies seeded?
+    // TODO: make this a function
+    SeedHelper.tableDep[TableName].forEach(async (dependencyTable: string) => {
+      if (tableRowIds[dependencyTable]) {
+        isTableReady = isTableReady && true;
+        // console.log(dependencyTable, " has rows");
+      } else {
+        isTableReady = isTableReady && false;
+        // console.log(dependencyTable, " has no rows");
+      }
+    });
+    console.log("All dependencies seeded?: ", isTableReady);
+    if (isTableReady) {
+      const count: number = await (unGuardedPrisma as any)[tableName].count(); // TODO: put in a try and catch
+      console.log("Count before insertion: ", count);
+      // await (unGuardedPrisma as any)[tableName].deleteMany();
+      const size = 2000; // replace with your desired size
+      const dataArray = await Promise.all(
+        new Array(size).fill(null).map(async (data: Object) => {
+          // console.log("starting an iteration");
+          data = SeedHelper.functions["fake" + TableName + "Complete"]();
+          await SeedHelper.fieldOverride(data);
+          // console.log("ending an iteration");
+
+          Object.keys(SeedHelper.tableRefs[TableName]).forEach((key) => {
+            //   console.log("key: ", key);
+            //   console.log("value: ", SeedHelper.tableRefs[TableName][key]);
+            const array = tableRowIds[SeedHelper.tableRefs[TableName][key]];
+            const randomIndex = Math.floor(Math.random() * array.length);
+            data[key] = array[randomIndex]["id"];
+          });
+
+          Object.keys(data).forEach((key) => {
+            if (Number.isInteger(data[key])) {
+              if (data[key] > 2147483647 || data[key] < -2147483648) {
+                // console.error(
+                //   `Value ${data[key]} at key ${key} is too large for an INT4 field.`,
+                // );
+                data[key] = Math.floor(Math.random() * 2147483647);
+                // console.info(`${key} is reassigned to ${data[key]}.`);
+              }
+            }
+            if (Array.isArray(data[key])) {
+              data[key] = data[key].map((value: number) => {
+                if (Number.isInteger(value)) {
+                  if (value > 2147483647 || value < -2147483648) {
+                    // console.error(
+                    //   `Value ${value} at key ${key} array is too large for an INT4 field.`,
+                    // );
+                    value = Math.floor(Math.random() * 2147483647);
+                    // console.info(
+                    //   `Element of ${key} is reassigned to ${value}.`,
+                    // );
+                  }
+                }
+                return value;
+              });
+            }
+          });
+          return data;
+        }),
+      );
+      try {
+        console.log("pushing data for ", TableName);
+        const CreateResults = await (unGuardedPrisma as any)[
+          tableName
+        ].createMany({
+          data: dataArray,
+          skipDuplicates: true,
+        });
+        console.log(
+          "Added ",
+          CreateResults.count,
+          " rows to ",
+          TableName,
+          " table",
+        );
+        tableRowIds[TableName] = await (unGuardedPrisma as any)[
+          tableName
+        ].findMany({
+          select: { id: true },
+        });
+      } catch (error) {
+        console.log(
+          "Error with table ",
+          tableName,
+          "\nhere is the error:\n",
+          error,
+          "\nhere is the data:\n",
+          dataArray,
+        );
+      }
+    }
+  }
+}
+
+console.log("completed seeding");
--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -1,7 +1,7 @@
-import { PrismaClient } from '@prisma/client'
-import { enhance } from '@zenstackhq/runtime'
-import { init, isCuid } from '@paralleldrive/cuid2'
-import { warn } from 'console'
+import { PrismaClient } from "@prisma/client";
+import { enhance } from "@zenstackhq/runtime";
+import { init, isCuid } from "@paralleldrive/cuid2";
+import { warn } from "console";
 
 // The init function returns a custom createId function with the specified
 // configuration. All configuration properties are optional.
@@ -13,13 +13,13 @@ const createId = init({
   length: 10,
   // A custom fingerprint for the host environment. This is used to help
   // prevent collisions when generating ids in a distributed system.
-  fingerprint: 'a-custom-host-fingerprint',
-})
+  fingerprint: "a-custom-host-fingerprint",
+});
 
 // Hard-code a unique key, so we can look up the client when this module gets re-imported
 const unGuardedPrisma = new PrismaClient({
   // log: ['query', 'info', 'warn', 'error'],
-  errorFormat: 'pretty',
+  errorFormat: "pretty",
 }).$extends({
   // model: {
   //   $allModels: {
@@ -33,51 +33,52 @@ const unGuardedPrisma = new PrismaClient({
     $allModels: {
       async create({ model, operation, args, query }) {
         // check if id exists and is valid cuid
+        // TODO: Test this with createMany
         if (args.data.id != null && isCuid(args.data.id)) {
-          console.log('CUID2: Adding cuid2 to data')
-          args.data.id = createId()
+          console.log("CUID2: Adding cuid2 to data");
+          args.data.id = createId();
         }
 
-        return query(args)
+        return query(args);
       },
       async createMany({ model, operation, args, query }) {
         if (Array.isArray(args.data)) {
           args.data.map((entry) => {
-            if (entry.id != null && isCuid(entry.id)) {
-              console.log('CUID2: Adding cuid2 to array data')
-              entry.id = createId()
+            if (entry.id == null && !isCuid(entry.id)) {
+              console.log("CUID2: Adding cuid2 to array data");
+              entry.id = createId();
             }
-            return entry
-          })
+            return entry;
+          });
         } else {
-          if (args.data.id != null && isCuid(args.data.id)) {
-            console.log('CUID2: Adding cuid2 to data')
-            args.data.id = createId()
+          if (args.data.id == null && !isCuid(args.data.id)) {
+            console.log("CUID2: Adding cuid2 to data");
+            args.data.id = createId();
           }
         }
 
-        return query(args)
+        return query(args);
       },
     },
   },
-})
+});
 
-unGuardedPrisma.$connect()
+unGuardedPrisma.$connect();
 
 function enhancedPrisma(userId?: string) {
   if (!userId) {
-    console.log(warn('userId provided to enhancedPrisma is null'))
+    console.log(warn("userId provided to enhancedPrisma is null"));
     return enhance(
       unGuardedPrisma,
-      {}
+      {},
       // { logPrismaQuery: true }
-    )
+    );
   }
   return enhance(
     unGuardedPrisma,
-    { user: { id: userId } }
+    { user: { id: userId } },
     // { logPrismaQuery: true }
-  )
+  );
 }
 
-export { unGuardedPrisma, enhancedPrisma }
+export { unGuardedPrisma, enhancedPrisma };
