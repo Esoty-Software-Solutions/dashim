3362b08cc588caeabdc9fcbd3afa5661aa2e31a5
Rasheed <rashed90ataher.90@gmail.com>
2024-01-22 16:20:22 +0200

    feat: adjust layout template

---
+++ browser/src/modules/institution/Filter.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const colorIsClicked = ref(false);
+
+function test() {
+  colorIsClicked.value = !colorIsClicked.value;
+  console.log(colorIsClicked.value);
+}
+</script>
+
+<template>
+  <VCard width="300">
+    <v-row no-gutters :class="colorIsClicked ? undefined : 'bg-primary'">
+      <v-col cols="1">
+        <v-sheet
+          :color="colorIsClicked ? 'primary' : undefined"
+          color-is-clicked
+          height="40"
+          class="pa-2 ma-1"
+          @click="test"
+        />
+      </v-col>
+
+      <v-col cols="11">
+        <v-sheet class="ma-1">
+          <v-text-field hide-details />
+        </v-sheet>
+      </v-col>
+    </v-row>
+  </VCard>
+</template>
