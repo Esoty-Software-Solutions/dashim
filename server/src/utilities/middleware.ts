69dc5491fab8f48a5d1bd46638888dfb0395804c
Abdul Fourteia <raoph40@gmail.com>
2024-01-22 13:11:01 +0200

    disabling auth for now

--- server/src/utilities/middleware.ts
+++ server/src/utilities/middleware.ts
@@ -6,65 +6,66 @@ Order of calls are
 3. HOF1
 4. originalFunction
 */
-import type { Request as ExpressRequest } from 'express'
-import { getUserFromDb } from '@utilities/auth'
+import type { Request as ExpressRequest } from "express";
+import { getUserFromDb } from "@utilities/auth";
 
 export type MiddlewareContext = {
-  bypassMiddleware?: boolean
-}
+  bypassMiddleware?: boolean;
+};
 
 export function middleware<T extends (...args: any[]) => any>(
   originalMethod: T,
   defaultContext: MiddlewareContext = {
     bypassMiddleware: false,
-  }
+  },
 ) {
   return async function (
     request: Request | ExpressRequest,
     args: Parameters<T>[1] = {},
     _defaultContext: MiddlewareContext = {
       bypassMiddleware: false,
-    }
+    },
   ): Promise<ReturnType<T> | null> {
     // console.log('request', request)
-    console.log(`Middleware: Entering ${originalMethod.name}`)
-    console.log('originalMethod name', originalMethod.name)
-    const context = { ...defaultContext, ..._defaultContext }
+    console.log(`Middleware: Entering ${originalMethod.name}`);
+    console.log("originalMethod name", originalMethod.name);
+    const context = { ...defaultContext, ..._defaultContext };
     // console.log('context is: ', context)
     if (context.bypassMiddleware) {
-      console.log('bypassMiddleware is true')
-      return await originalMethod(null, args)
+      console.log("bypassMiddleware is true");
+      return await originalMethod(null, args);
     } else {
-      console.log('bypassMiddleware is false')
+      console.log("bypassMiddleware is false");
       try {
-        const user = await getUserFromDb(request)
-        return await originalMethod(user.id, args)
+        return await originalMethod("placeHolder", args); // TODO: fix authentication
+        // const user = await getUserFromDb(request);
+        // return await originalMethod(user.id, args)
       } catch (error) {
-        console.error('**** Error at middleware ****')
-        throw error
+        console.error("**** Error at middleware ****");
+        throw error;
       }
 
       // Assuming the userId is the first argument of originalMethod
     }
-  }
+  };
 }
 
 export async function log() {
-  console.log('log')
-  return null
+  console.log("log");
+  return null;
 }
 
 export async function validate() {
-  console.log('validate')
-  return null
+  console.log("validate");
+  return null;
 }
 
 export async function errorHandler() {
-  console.log('errorHandler')
-  return null
+  console.log("errorHandler");
+  return null;
 }
 
 export async function rateLimiter(request: Request) {
-  console.log('rateLimiter')
-  return null
+  console.log("rateLimiter");
+  return null;
 }
