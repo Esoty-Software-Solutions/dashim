e26b81b3f4f05f28e5aa5edda774d4e6e843b8c3
Abdul Fourteia <raoph40@gmail.com>
2024-01-31 18:34:25 +0200

    removed trpc input types from the routerCO template. validation still applies

--- server/scripts/routerCOFileTemplate.ts
+++ server/scripts/routerCOFileTemplate.ts
@@ -21,82 +21,89 @@ import {
 
 export const userRouter = router({
   // aggregate: publicProcedure
-  //   .input(UserAggregateSchema)
+  //   //.input(UserAggregateSchema)
   //   .query(async ({ ctx, input }) => {
   //     try {
-  //       return await ctx.prisma.user.aggregate(input);
+  //       return await ctx.prisma.user.aggregate(UserAggregateSchema.parse(input));
   //     } catch (error) {
   //       throwCustomError(error);
   //     }
   //   }),
 
   createMany: publicProcedure
-    .input(UserCreateManySchema)
+    //.input(UserCreateManySchema)
     .mutation(async ({ ctx, input }) => {
       try {
-        return await ctx.prisma.user.createMany(input);
+        return await ctx.prisma.user.createMany(
+          UserCreateManySchema.parse(input),
+        );
       } catch (error) {
         throwCustomError(error);
       }
     }),
 
   createOne: publicProcedure
-    .input(UserCreateOneSchema)
+    //.input(UserCreateOneSchema)
     .mutation(async ({ ctx, input }) => {
       try {
-        return await ctx.prisma.user.create(input);
+        return await ctx.prisma.user.create(UserCreateOneSchema.parse(input));
       } catch (error) {
         throwCustomError(error);
       }
     }),
 
   deleteMany: publicProcedure
-    .input(UserDeleteManySchema)
+    //.input(UserDeleteManySchema)
     .mutation(async ({ ctx, input }) => {
       try {
-        return await ctx.prisma.user.deleteMany(input);
+        return await ctx.prisma.user.deleteMany(
+          UserDeleteManySchema.parse(input),
+        );
       } catch (error) {
         throwCustomError(error);
       }
     }),
 
   deleteOne: publicProcedure
-    .input(UserDeleteOneSchema)
+    //.input(UserDeleteOneSchema)
     .mutation(async ({ ctx, input }) => {
       try {
-        return await ctx.prisma.user.delete(input);
+        return await ctx.prisma.user.delete(UserDeleteOneSchema.parse(input));
       } catch (error) {
         throwCustomError(error);
       }
     }),
 
   findFirst: publicProcedure
-    .input(UserFindFirstSchema)
+    //.input(UserFindFirstSchema)
     .query(async ({ ctx, input }) => {
       try {
-        return await ctx.prisma.user.findFirst(input);
+        return await ctx.prisma.user.findFirst(
+          UserFindFirstSchema.parse(input),
+        );
       } catch (error) {
         throwCustomError(error);
       }
     }),
 
   // findFirstOrThrow: publicProcedure
-  //   .input(UserFindFirstSchema)
+  //   //.input(UserFindFirstSchema)
   //   .query(async ({ ctx, input }) => {
   //     try {
-  //       return await ctx.prisma.user.findFirstOrThrow(input);
+  //       return await ctx.prisma.user.findFirstOrThrow(.parse(input));
   //     } catch (error) {
   //       throwCustomError(error);
   //     }
   //   }),
 
   findMany: publicProcedure
-    .input(UserFindManySchema)
+    //.input(UserFindManySchema)
     .query(async ({ ctx, input }) => {
+      const validInput = UserFindManySchema.parse(input);
       try {
         const [data, filteredCount, unFilteredCount] = await Promise.all([
-          ctx.prisma.user.findMany(input),
-          ctx.prisma.user.count({ where: input?.where }),
+          ctx.prisma.user.findMany(validInput),
+          ctx.prisma.user.count({ where: validInput?.where }),
           ctx.prisma.user.count(),
         ]);
         const statistics: {
@@ -115,27 +122,31 @@ export const userRouter = router({
     }),
 
   findUnique: publicProcedure
-    .input(UserFindUniqueSchema)
+    //.input(UserFindUniqueSchema)
     .query(async ({ ctx, input }) => {
       try {
-        return await ctx.prisma.user.findUnique(input);
+        return await ctx.prisma.user.findUnique(
+          UserFindUniqueSchema.parse(input),
+        );
       } catch (error) {
         throwCustomError(error);
       }
     }),
 
   findUniqueOrThrow: publicProcedure
-    .input(UserFindUniqueSchema)
+    //.input(UserFindUniqueSchema)
     .query(async ({ ctx, input }) => {
       try {
-        return ctx.prisma.user.findUniqueOrThrow(input);
+        return ctx.prisma.user.findUniqueOrThrow(
+          UserFindUniqueSchema.parse(input),
+        );
       } catch (error) {
         throwCustomError(error);
       }
     }),
 
   // groupBy: publicProcedure
-  //   .input(UserGroupBySchema)
+  //   //.input(UserGroupBySchema)
   //   .query(async ({ ctx, input }) => {
   //     try {
   //       return await ctx.prisma.user.groupBy({
@@ -152,40 +163,40 @@ export const userRouter = router({
   //   }),
 
   // updateMany: publicProcedure
-  //   .input(UserUpdateManySchema)
+  //   //.input(UserUpdateManySchema)
   //   .mutation(async ({ ctx, input }) => {
   //     try {
-  //       return await ctx.prisma.user.updateMany(input);
+  //       return await ctx.prisma.user.updateMany(UserUpdateManySchema.parse(input));
   //     } catch (error) {
   //       throwCustomError(error);
   //     }
   //   }),
 
   updateOne: publicProcedure
-    .input(UserUpdateOneSchema)
+    //.input(UserUpdateOneSchema)
     .mutation(async ({ ctx, input }) => {
       try {
-        return await ctx.prisma.user.update(input);
+        return await ctx.prisma.user.update(UserUpdateOneSchema.parse(input));
       } catch (error) {
         throwCustomError(error);
       }
     }),
 
   // upsertOne: publicProcedure
-  //   .input(UserUpsertSchema)
+  //   //.input(UserUpsertSchema)
   //   .mutation(async ({ ctx, input }) => {
   //     try {
-  //       return await ctx.prisma.user.upsert(input);
+  //       return await ctx.prisma.user.upsert(.parse(input));
   //     } catch (error) {
   //       throwCustomError(error);
   //     }
   //   }),
 
   count: publicProcedure
-    .input(UserCountSchema)
+    //.input(UserCountSchema)
     .query(async ({ ctx, input }) => {
       try {
-        return await ctx.prisma.user.count(input);
+        return await ctx.prisma.user.count(UserCountSchema.parse(input));
       } catch (error) {
         throwCustomError(error);
       }
