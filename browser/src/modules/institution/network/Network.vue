55aa143bb0551f4594330f87834e1de82f582a3c
Mohamed <abedaliby@yahoo.com>
2024-02-14 14:43:20 +0200

    network list view complete

--- browser/src/modules/institution/network/Network.vue
+++ browser/src/modules/institution/network/Network.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import { ref, toRefs, computed, watch } from "vue";
+import { ref, toRefs, computed, watch, watchEffect } from "vue";
 import { useI18n } from "vue-i18n";
 
 import { mdiPlus } from "@mdi/js";
 
-import useBeneficiariesStore from "../stores/networkStore";
+import useNetworkStore from "../stores/networkStore";
 
 import DataPageBase from "@/modules/dataPage/DataPageSplitLayout.vue";
 import useDataFilters, { text } from "@/modules/filter/composables/dataFilter";
@@ -12,10 +12,10 @@ import useDataFilters, { text } from "@/modules/filter/composables/dataFilter";
 import type { TableHeader } from "@/modules/shared/interfaces";
 
 defineOptions({
-  name: "InstitutionsBeneficiariesPage",
+  name: "InstitutionsNetworkPage",
 });
 const { t } = useI18n();
-const store = toRefs(useBeneficiariesStore());
+const store = toRefs(useNetworkStore());
 // console.log(store.binding.value.items);
 let selected = ref([""]);
 let selectedCount = ref(0);
@@ -37,14 +37,39 @@ const { FilterComponent } = useDataFilters({
     }),
   },
 });
+const { FilterComponent: instituteFilter } = useDataFilters({
+  sheetProps: {
+    elevation: 0,
+  },
+  filter: {
+    firstName: text({
+      value: store.nameFilter,
+      enabled: store.nameFilterEnabled,
+      enableOnFocus: false,
+      props: {
+        label: "Name",
+      },
+      display: {
+        md: 8,
+      },
+    }),
+  },
+});
 
 watch(
-  () => ({ ...store.binding.value }),
+  () => ({ ...store.medicalCenterBinding.value }),
   () => {
     console.log("items updated");
     selected.value = [];
     selectedCount.value = 0;
   },
+);
+watchEffect(
+  () =>
+    (store.insurancePolicyMedicalCenterIds.value =
+      store.insurancePolicyMedicalCenterItems.value.map(
+        (item) => item.medicalCenterId,
+      )),
 );
 // actions
 function onSelected($event) {
@@ -54,21 +79,40 @@ function onSelected($event) {
   selectedCount.value = selected.value.length;
 }
 function refresh() {
-  store.triggerFetch.value();
+  store.medicalCenterTriggerFetch.value();
 }
 function selectAll() {
-  selected.value = store.binding.value.items.map((item) => item.id);
-  selectedCount.value = store.binding.value.itemsLength;
+  selected.value = store.medicalCenterBinding.value.items.map(
+    (item) => item.id,
+  );
+  selectedCount.value = store.medicalCenterBinding.value.itemsLength;
 
   console.log(selected.value);
 }
-function paginated() {
-  console.log("paginated");
-  selected.value = [];
-  selectedCount.value = 0;
-}
+// function handleClick(event, row) {
+//   store.selectedInstitutionId.value = row.item.id;
+//   // console.log("Clicked item: ", row.item.id);
+// }
 
 // table headers
+const institutionHeaders = ref<TableHeader[]>([
+  {
+    title: t("common.name"),
+    key: "name",
+    cellProps: () => {
+      return { dir: "auto" };
+    },
+  },
+]);
+const insurancePoliciesHeaders = ref<TableHeader[]>([
+  {
+    title: t("common.name"),
+    key: "name",
+    cellProps: () => {
+      return { dir: "auto" };
+    },
+  },
+]);
 const headers = ref<TableHeader[]>([
   {
     title: t("common.name"),
@@ -84,35 +128,148 @@ const headers = ref<TableHeader[]>([
     width: "1rem",
     align: "center",
   },
+  {
+    title: t("common.creationDate"),
+    key: "createdAt",
+    sortable: false,
+    width: "2.7rem",
+  },
+  {
+    title: t("common.updatedAt"),
+    key: "updatedAt",
+    sortable: false,
+    width: "2.7rem",
+  },
+  {
+    title: t("common.status"),
+    key: "isActive",
+    sortable: false,
+    align: "center",
+    width: "0.5rem",
+  },
 ]);
 </script>
 
 <template>
-  <DataPageBase>
-    <template #filters>
-      <FilterComponent />
-    </template>
-    <template #actions>
-      <VCardActions>
-        {{ selectedCount }}
-        <VBtn @click="selectAll">Select All</VBtn>
-        <VBtn @click="refresh">refresh</VBtn>
-        <VSpacer />
-        <VBtn color="primary" variant="plain">
-          <span>{{ t("institution.network.newbeneficiary") }}</span>
-          <VIcon end :icon="mdiPlus" />
-        </VBtn>
-      </VCardActions>
-    </template>
-    <template #table>
-      <VDataTableServer
-        v-model="selected"
-        :item-value="(item) => item.id"
-        :headers="headers"
-        v-bind="store.institutionBinding.value"
-        @input="onSelected($event)"
-      >
-        <!-- <template #item.name="{ item }">
+  <VContainer
+    ref="container"
+    v-bind="$attrs"
+    fluid
+    class="d-flex px-2 py-1 ga-1 flex-column h-100"
+  >
+    <VDefaultsProvider
+      :defaults="{
+        VSheet: {
+          elevation: 1,
+        },
+        VDataTableServer: {
+          class: ['elevation-1'],
+        },
+        VCol: {
+          class: ['m-1'],
+        },
+      }"
+    >
+      <VRow>
+        <VCol cols="4">
+          <VDefaultsProvider
+            :defaults="{
+              VDataTableServer: {
+                class: ['mt-1'],
+                style: 'flex: 1 1 0; min-height: 0;',
+                fixedHeader: true,
+                fixedFooter: true,
+                hover: true,
+                color: 'primary',
+              },
+            }"
+          >
+            <!-- institutions  table -->
+
+            <VDataTableServer
+              :item-value="(item) => item.id"
+              :headers="institutionHeaders"
+              v-bind="store.institutionBinding.value"
+              @click:row="
+                (event, row) => {
+                  store.selectedInstitutionId.value = row.item.id;
+                }
+              "
+            >
+              <template #item.name="{ item }">
+                <tr
+                  :class="{
+                    'text-primary':
+                      item.id == store.selectedInstitutionId.value,
+                  }"
+                >
+                  <td>{{ item.name }}</td>
+                </tr>
+              </template>
+              <template #bottom />
+            </VDataTableServer>
+            <!-- insurance policies table -->
+            <VDataTableServer
+              v-if="store.selectedInstitutionId.value"
+              :item-value="(item) => item.id"
+              :headers="insurancePoliciesHeaders"
+              v-bind="store.insurancePoliciesBinding.value"
+              @click:row="
+                (event, row) => {
+                  store.selectedInsurancePolicyId.value = row.item.id;
+                }
+              "
+            >
+              <template #item.name="{ item }">
+                <tr
+                  :class="{
+                    'text-primary':
+                      item.id == store.selectedInsurancePolicyId.value,
+                  }"
+                >
+                  <td>{{ item.name }}</td>
+                </tr>
+              </template>
+              <template #bottom />
+            </VDataTableServer>
+          </VDefaultsProvider> </VCol
+        ><VCol>
+          <VSheet>
+            <instituteFilter />
+          </VSheet>
+          <VSheet class="my-1">
+            <VCardActions>
+              {{ selectedCount }}
+              <VBtn @click="selectAll">Select All</VBtn>
+              <VBtn @click="refresh">refresh</VBtn>
+              <VSpacer />
+              <VBtn color="primary" variant="plain">
+                <span>{{ t("institution.network.newMedicalCenter") }}</span>
+                <VIcon end :icon="mdiPlus" />
+              </VBtn>
+            </VCardActions>
+          </VSheet>
+          <VDataTableServer
+            v-model="selected"
+            show-select
+            :item-value="(item) => item.id"
+            :headers="headers"
+            v-bind="store.medicalCenterBinding.value"
+            @input="onSelected($event)"
+          >
+            <template #item.createdAt="{ item }">
+              {{ new Date(item.createdAt).toLocaleDateString() }}
+            </template>
+            <template #item.isActive="{ item }">
+              <VChip :color="item.isActive ? 'primary' : 'error'">
+                {{ item.isActive ? "Active" : "Inactive" }}
+              </VChip>
+            </template>
+
+            <template #item.actions>
+              <VIcon :icon="mdiPlus" />
+            </template>
+            <!-- <template #item.name="{ item }">
           {{ item.firstName }} {{ item.secondName }} {{ item.thirdName }}
           {{ item.lastName }}
         </template>
@@ -130,8 +287,9 @@ const headers = ref<TableHeader[]>([
             {{ item.isActive ? "Active" : "Inactive" }}
           </VChip>
         </template> -->
-        <template #bottom />
-      </VDataTableServer>
-    </template>
-  </DataPageBase>
+          </VDataTableServer>
+        </VCol>
+      </VRow>
+    </VDefaultsProvider>
+  </VContainer>
 </template>
--- browser/src/modules/institution/stores/networkStore.ts
+++ browser/src/modules/institution/stores/networkStore.ts
@@ -1,6 +1,6 @@
 import { useLocalStorage } from "@vueuse/core";
 import { defineStore } from "pinia";
-import { ref, computed } from "vue";
+import { ref, computed, watch, watchEffect } from "vue";
 
 import useQuerierTable from "@/modules/shared/composables/useQuerierTable";
 import { client } from "@/queries";
@@ -13,38 +13,26 @@ const useNetworkStore = defineStore("NetworkStoreList", () => {
     "networkList.nameFilterEnabled",
     true,
   );
-  // const getSubs = async ()=>{
-  // let subs =  await client.procedure.listSubscribers.query()
-  //  if(subs?.data){
-  //   subs.data.forEach(element => {
-  //     element.network.forEach(element => {
-  //       element
-  //     })
-  //     element.
-  //   })
-  //  }
-  // }
+  const selectedInstitutionId = ref("");
+  const selectedInsurancePolicyId = ref("");
+  const insurancePolicyMedicalCenterIds = ref<string[] | never[]>([]);
+
   // if using "immediate=true"
   // the table will to hit the api without the need to change dependant
   // the first fetch is when the filters/page change
-  const { binding, items, triggerFetch } = useQuerierTable({
+  const {
+    binding: institutionBinding,
+    items: institutionItems,
+    triggerFetch: institutionTriggerFetch,
+  } = useQuerierTable({
     storageKey: "networkList",
     input: () => {
-      // type inputType = Parameters<typeof client.crud.beneficiary.findMany.query>[0]
-      // const where: any = {};
-
-      if (nameFilterEnabled.value && nameFilter.value.trim()) {
-        // searchName : { contains: nameFilter.value.trim() }
-        return {
-          where: { searchName: { contains: nameFilter.value.trim() } },
-        };
-      }
       return {
         where: {},
       };
     },
     // findCallback: client.procedure.listSubscribers.query,
-    findCallback: client.crud.beneficiary.findMany.query,
+    findCallback: client.crud.institution.findMany.query,
     onError(error) {
       globalStore.setMessage("Error while connection to server.");
       console.error(error);
@@ -53,19 +41,19 @@ const useNetworkStore = defineStore("NetworkStoreList", () => {
   });
 
   const {
-    binding: institutionBinding,
-    items: institutionItems,
-    triggerFetch: institutionTriggerFetch,
+    binding: insurancePoliciesBinding,
+    items: insurancePoliciesItems,
+    triggerFetch: insurancePoliciesTriggerFetch,
   } = useQuerierTable({
     storageKey: "networkList",
     input: () => {
       // type inputType = Parameters<typeof client.crud.beneficiary.findMany.query>[0]
       // const where: any = {};
 
-      if (nameFilterEnabled.value && nameFilter.value.trim()) {
+      if (selectedInstitutionId.value) {
         // searchName : { contains: nameFilter.value.trim() }
         return {
-          where: { searchName: { contains: nameFilter.value.trim() } },
+          where: { institutionId: selectedInstitutionId.value },
         };
       }
       return {
@@ -73,7 +61,7 @@ const useNetworkStore = defineStore("NetworkStoreList", () => {
       };
     },
     // findCallback: client.procedure.listSubscribers.query,
-    findCallback: client.crud.intitution.findMany.query,
+    findCallback: client.crud.insurancePolicy.findMany.query,
     onError(error) {
       globalStore.setMessage("Error while connection to server.");
       console.error(error);
@@ -83,17 +71,103 @@ const useNetworkStore = defineStore("NetworkStoreList", () => {
   // &_& depending on ts performance and inference capabilities
   // `items`, `input` should have the proper types
 
+  const {
+    binding: insurancePolicyMedicalCenterBinding,
+    items: insurancePolicyMedicalCenterItems,
+    triggerFetch: insurancePolicyMedicalCenterTriggerFetch,
+  } = useQuerierTable({
+    storageKey: "networkList",
+    input: () => {
+      // type inputType = Parameters<typeof client.crud.beneficiary.findMany.query>[0]
+      // const where: any = {};
+      // if (nameFilterEnabled.value && nameFilter.value.trim()) {
+      //   // searchName : { contains: nameFilter.value.trim() }
+      //   return {
+      //     // where: {
+      //     //   name: { contains: nameFilter.value.trim() },
+      //     //   insurancePolicy: {
+      //     //     id: selectedInsurancePolicyId.value
+      //     //       ? selectedInsurancePolicyId.value
+      //     //       : undefined,
+      //     //   },
+      //     // },
+      //   };
+      // }
+      return {
+        where: {
+          insurancePolicy: {
+            id: selectedInsurancePolicyId.value
+              ? selectedInsurancePolicyId.value
+              : undefined,
+          },
+        },
+      };
+    },
+    // findCallback: client.procedure.listSubscribers.query,
+    findCallback: client.crud.insurancePolicyMedicalCenter.findMany.query,
+    onError(error) {
+      globalStore.setMessage("Error while connection to server.");
+      console.error(error);
+    },
+    immediate: true,
+  });
+  const {
+    binding: medicalCenterBinding,
+    items: medicalCenterItems,
+    triggerFetch: medicalCenterTriggerFetch,
+  } = useQuerierTable({
+    storageKey: "networkList",
+    input: () => {
+      // type inputType = Parameters<typeof client.crud.beneficiary.findMany.query>[0]
+      // const where: any = {};
+      // if (nameFilterEnabled.value && nameFilter.value.trim()) {
+      // searchName : { contains: nameFilter.value.trim() }
+      return {
+        where: {
+          id: { in: insurancePolicyMedicalCenterIds.value },
+          name:
+            nameFilterEnabled.value && nameFilter.value.trim()
+              ? { contains: nameFilter.value.trim() }
+              : undefined,
+
+          //   name: { contains: nameFilter.value.trim() },
+          //   insurancePolicy: {
+          //     id: selectedInsurancePolicyId.value
+          //       ? selectedInsurancePolicyId.value
+          //       : undefined,
+          //   },
+        },
+      };
+      // }
+    },
+    // findCallback: client.procedure.listSubscribers.query,
+    findCallback: client.crud.medicalCenter.findMany.query,
+    onError(error) {
+      globalStore.setMessage("Error while connection to server.");
+      console.error(error);
+    },
+    immediate: true,
+  });
   return {
     idSearch,
     idEnabled,
     nameFilter,
     nameFilterEnabled,
-    binding,
-    items,
-    triggerFetch,
+    selectedInstitutionId,
+    selectedInsurancePolicyId,
+    medicalCenterBinding,
+    medicalCenterItems,
+    medicalCenterTriggerFetch,
     institutionBinding,
     institutionItems,
     institutionTriggerFetch,
+    insurancePoliciesBinding,
+    insurancePoliciesItems,
+    insurancePoliciesTriggerFetch,
+    insurancePolicyMedicalCenterBinding,
+    insurancePolicyMedicalCenterItems,
+    insurancePolicyMedicalCenterTriggerFetch,
+    insurancePolicyMedicalCenterIds,
   };
 });
 
