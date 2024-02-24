b00a0c759dcc2159688e6487083ea718b9439a4d
Abdul Fourteia <raoph40@gmail.com>
2024-02-24 18:09:11 +0200

    now searchName is populated

--- server/src/procedures/beneficiary/beneficiary.procedure.action.ts
+++ server/src/procedures/beneficiary/beneficiary.procedure.action.ts
@@ -15,12 +15,30 @@ export const actions = {
           data: beneficiaryEntity.data.beneficiaries.map((beneficiary) => ({
             ...beneficiary,
             statusSetById: userId,
-            searchName: "", //TODO: this is a computed field so it should be optional
+            searchName: this.normalizeSearchName(
+              [
+                beneficiary.firstName,
+                beneficiary.secondName,
+                beneficiary.thirdName,
+                beneficiary.fourthName,
+                beneficiary.lastName,
+              ]
+                .filter((name) => name) // Remove any empty or undefined names
+                .map((name) => (name ? name.replace(/\s/g, "") : "")) // Trim whitespace from each name
+                .join(" "), // Concatenate the names with a space
+            ), //TODO: this is a computed field so it should be optional
           })),
         },
       },
     };
 
     return formattedData;
+  },
+  normalizeSearchName: function (name: string) {
+    return name
+      .trim()
+      .replace(/\s+/g, " ")
+      .toLowerCase()
+      .replace(/[^a-z0-9\s]/g, "");
   },
 };
