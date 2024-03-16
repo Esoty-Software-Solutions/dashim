d74f3f1f85ff28964c1e5cbaa533e3ebc38d9bb9
Abdul Fourteia <raoph40@gmail.com>
2024-03-17 01:30:21 +0200

    testing json-rules-engine

--- server/src/playground/test.ts
+++ server/src/playground/test.ts
@@ -1,166 +1,245 @@
-// Original function (OG)
-function addS(a: number, b: number): string {
-  return (a + b).toString()
-}
+// import { actions } from "@procedures/_actions";
+// import ServerError from "@utilities/error";
+import util from "util";
+import {
+  Engine,
+  Rule,
+  Fact,
+  Event,
+  RuleProperties,
+  TopLevelCondition,
+  FactOptions,
+  DynamicFactCallback,
+  Almanac,
+} from "json-rules-engine";
+import { Beneficiary } from "@prisma/client";
 
-function addN(a: number, b: number): number {
-  return a + b
+interface ConditionProperties {
+  fact: FactKeys;
+  operator:
+    | "equal"
+    | "notEqual"
+    | "greaterThan"
+    | "lessThan"
+    | "greaterThanInclusive"
+    | "lessThanInclusive"
+    | "in"
+    | "notIn"
+    | "contains"
+    | "doesNotContain";
+  value: { fact: FactKeys } | any;
+  path?: string;
+  priority?: number;
+  params?: Record<string, any>;
+  name?: string;
 }
+type FactKeys = keyof typeof facts;
 
-// Higher-Order Function (HOF)
-function HOF1(
-  OG: (a: number, b: number) => T,
-  defaultNumber: number = 10
-): (a: number, b: number) => T {
-  return (a: number, b: number): T | number => {
-    return OG(a, b) + defaultNumber
+const facts = {
+  selfRelationshipOccurrence: async (
+    params: Record<string, any>,
+    almanac: Almanac,
+  ) => {
+    return almanac.factValue("beneficiaryObject").then((value: any) => {
+      return value.filter((element: any) => element.relationship === "self")
+        .length as number;
+    });
+  },
+  fatherRelationshipOccurrence: async (
+    params: Record<string, any>,
+    almanac: Almanac,
+  ) => {
+    return almanac.factValue("beneficiaryObject").then((value: any) => {
+      return value.filter((element: any) => element.relationship === "father")
+        .length as number;
+    });
+  },
+  motherRelationshipOccurrence: async (
+    params: Record<string, any>,
+    almanac: Almanac,
+  ) => {
+    return almanac.factValue("beneficiaryObject").then((value: any) => {
+      return value.filter((element: any) => element.relationship === "mother")
+        .length as number;
+    });
+  },
+  test: async (params: Record<string, any>, almanac: Almanac) => {
+    return 1;
+  },
+};
+
+const keys = Object.keys(facts) as FactKeys[];
+
+const conditions: TopLevelCondition = {
+  any: [
+    {
+      fact: "selfRelationshipOccurrence",
+      operator: "equal",
+      value: 0,
+      priority: 1,
+      name: "one self relationship required",
+    },
+    {
+      fact: "fatherRelationshipOccurrence",
+      operator: "lessThanInclusive",
+      value: 0,
+      priority: 1,
+      name: "at most one father",
+    },
+    {
+      fact: "motherRelationshipOccurrence",
+      operator: "lessThanInclusive",
+      value: 0,
+      priority: 4,
+      name: "at most one mother",
+    },
+    {
+      all: [
+        {
+          fact: "test",
+          operator: "greaterThanInclusive",
+          value: 5,
+          priority: 1,
+          name: "one test1 required",
+        },
+        {
+          fact: "test",
+          operator: "lessThanInclusive",
+          value: 555,
+          priority: 1,
+          name: "one test2 required",
+        },
+        {
+          not: {
+            fact: "test",
+            operator: "equal",
+            value: 1,
+            priority: 1,
+            name: "one test3 required",
+          },
+        },
+      ],
+    },
+  ],
+};
+
+const rules = new Rule({
+  name: "Beneficiary relationship check",
+  priority: 10,
+  conditions: conditions,
+  event: {
+    type: "beneficiaryRelationshipCheck",
+  },
+  onSuccess: (event, almanac, ruleResult) => {
+    // console.log("onSuccess", event, almanac, ruleResult);
+    console.log("onSuccess", event);
+    almanac.addFact("rulesPassed", true);
+  },
+  onFailure: (event, almanac, ruleResult) => {
+    //   // console.log("onFailure", event, almanac, ruleResult);
+    // console.log("onFailure", ruleResult);
+    almanac.addFact("rulesPassed", false);
+    almanac.addFact("failureReport", parseResult(ruleResult.conditions, false));
+  },
+});
+
+function parseResultObject(condition: any, resultType: boolean) {
+  if (condition.operator === "not") {
+    if (condition.result === resultType) {
+      return {
+        fact: condition.not.fact,
+        operator: `not ${condition.not.operator}`,
+        value: condition.not.value,
+        result: condition.not.result,
+      };
+    }
+  } else {
+    if (condition.result === resultType) {
+      return {
+        fact: condition.fact,
+        operator: condition.operator,
+        value: condition.value,
+        result: condition.result,
+      };
+    }
   }
 }
-
-function HOF2(
-  OG: (a: number, b: number) => number,
-  defaultNumber: number = 15
-): (a: number, b: number, defaultNumber?: number) => number {
-  return (a: number, b: number, defaultNumberOverride?: number): number => {
-    const finalDefaultNumber = defaultNumberOverride ?? defaultNumber
-    return OG(a, b) + finalDefaultNumber
+function parseResult(condition: any, resultType: boolean) {
+  if (condition.operator === "any" || condition.operator === "all") {
+    const x = condition[condition.operator]
+      .map((element: any) => {
+        return parseResult(element, resultType);
+      })
+      .filter((element: any) => element !== undefined);
+    return { [condition.operator]: x };
+  } else {
+    return parseResultObject(condition, resultType);
   }
 }
+export async function ruleCheck(beneficiary2?: Beneficiary[]) {
+  const engine = new Engine();
+  keys.forEach((key) => {
+    engine.addFact(key, facts[key]);
+  });
 
-function example<T>(arg: T): T {
-  return arg
+  const beneficiary = {
+    beneficiaryObject: [
+      {
+        id: "1",
+        relationship: "self",
+      },
+      {
+        id: "1",
+        relationship: "mother",
+      },
+      {
+        id: "1",
+        relationship: "mother",
+      },
+      {
+        id: "1",
+        relationship: "father",
+      },
+    ],
+  };
+  engine.addRule(rules);
+  let jsonString = rules.toJSON(false);
+  console.log(
+    "stringify rules object",
+    util.inspect(jsonString, false, null, true),
+  );
+  engine.on("failure", (event, almanac, ruleResult) => {
+    engine.stop();
+  });
+  const result = await engine.run(beneficiary);
+
+  console.log("rules result...", await result.almanac.factValue("rulesPassed"));
+  if (result.failureResults.length > 0) {
+    let failureReport: any = {};
+
+    // const oprtr = result.failureResults[0].conditions.operator;
+    // console.log("result.failureResults", result.failureResults[0].conditions);
+    failureReport = parseResult(result.failureResults[0].conditions, false);
+    console.log(
+      "failure report",
+      util.inspect(
+        await result.almanac.factValue("failureReport"),
+        false,
+        null,
+        true,
+      ),
+    );
+
+    // console.log(
+    //   "failure report",
+    //   util.inspect(failureReport, false, null, true),
+    // );
+  }
+
+  // const successReport = parseResult(result.results[0].conditions, true);
+  // console.log("failure report", util.inspect(successReport, false, null, true));
+
+  return { resolution: true, message: "Beneficiary relationship check passed" };
 }
 
-const x = 22 as number
-
-const ssss = example(x)
-
-console.log(ssss)
-
-// Call a function that passes OG into HOF and arguments for the OG function
-const newFunction11 = HOF1(addN)
-console.log(newFunction11(1, -1)) // Outputs: 13 (1 + 2 + 10)
-
-// Call another function that passes OG into HOF as well as arguments for OG function and second argument for HOF function
-const newFunction12 = HOF1(addS, 20)
-console.log(newFunction12(1, -1)) // Outputs: 23 (1 + 2 + 20)
-
-// Call a function that passes OG into HOF and arguments for the OG function
-const newFunction21 = HOF2(add, 4)
-console.log(newFunction21(1, -1)) // Outputs: 13 (1 + 2 + 4)
-
-// Call another function that passes OG into HOF as well as arguments for OG function and second argument for HOF function
-const newFunction22 = HOF2(add)
-console.log(newFunction22(1, -1, 12)) // Outputs: 15 (1 + 2 + 12)
-
-// Call another function that passes OG into HOF as well as arguments for OG function and second argument for HOF function
-const newFunction23 = HOF2(add)
-console.log(newFunction23(1, -1)) // Outputs: 15 (1 + 2 + 12)
-
-// export function HOF2<T extends (...args: any) => any>(
-//   OG: T,
-//   defaultParamHOF: { c?: string; d?: number } = { c: 'default2', d: -5 }
-// ) {
-//   console.log('creating HOF2%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
-//   return async function (
-//     args: Parameters<T>[0] = {},
-//     paramHOF: { c?: string; d?: number } = {}
-//   ) {
-//     console.log('entering HOF2')
-//     paramHOF = { ...defaultParamHOF, ...paramHOF }
-//     console.log(
-//       `HOF2 parameter: ${paramHOF.c ?? 'c is not defined'}, ${
-//         paramHOF.d ?? 'd is not defined'
-//       }`
-//     )
-//     args.a += paramHOF.d
-//     return await OG(args)
-//   }
-// }
-
-// export function HOF<T extends (...args: any) => any>(
-//   OG: T,
-//   defaultParamHOF: { c?: string; d?: number } = { c: 'default', d: 0 },
-//   defaultParamHOF1: { c?: string; d?: number } = { c: 'default', d: 0 }
-// ): (...funcArgs: Parameters<T>) => Promise<ReturnType<T>> {
-//   console.log('creating HOF#################################')
-//   const test = HOF2(OG)
-//   return async function (
-//     args: Parameters<T>[0],
-//     paramHOF: { c?: string; d?: number } = {},
-//     test1: { c?: string; d?: number } = {}
-//   ): Promise<ReturnType<T>> {
-//     console.log('entering HOF')
-//     console.log('OG name', OG.name)
-//     paramHOF = { ...defaultParamHOF, ...paramHOF }
-//     console.log(
-//       `HOF parameter: ${paramHOF.c ?? 'c is not defined'}, ${
-//         paramHOF.d ?? 'd is not defined'
-//       }`
-//     )
-//     // args.a += paramHOF.d
-//     return await OG(args)
-//     // return await test(args, paramHOF)
-//     // return await HOF2(OG)(args, paramHOF)
-//   }
-// }
-
-// // Original function (OG)
-// async function add({ a, b }: { a: number; b: number }) {
-//   return await (a + b)
-// }
-
-// async function add2({ a, b }: { a: number; b: number }) {
-//   return await (a + b)
-// }
-
-// // Create a new function with HOF
-// console.log('creating ADD----------')
-// const ADD = {
-//   newFunction: HOF(add),
-//   newFunction1: HOF(add2, { c: 'custom1', d: 40 }),
-// }
-// console.log('Done creating ADD----------')
-
-// console.log('creating newFunction2----------')
-
-// const newFunction2 = HOF(add)
-
-// console.log('Done creating newFunction2----------')
-
-// // Call the new function
-// async function run() {
-//   console.log('Outputs NF: ', await ADD.newFunction({ a: 5, b: 15 }))
-//   console.log(
-//     'Outputs NF: ',
-//     await ADD.newFunction({ a: 5, b: 15 }, { c: 'custom', d: 20 })
-//   )
-//   console.log(
-//     'Outputs NF: ',
-//     await ADD.newFunction({ a: 5, b: 15 }, { c: `stringhere` })
-//   )
-//   console.log('Outputs NF: ', await ADD.newFunction({ a: 5, b: 15 }, { d: 10 }))
-
-//   console.log('Outputs NF1: ', await ADD.newFunction1({ a: 5, b: 15 }))
-//   console.log(
-//     'Outputs NF1: ',
-//     await ADD.newFunction1({ a: 5, b: 15 }, { c: 'custom', d: 20 })
-//   )
-//   console.log(
-//     'Outputs NF1: ',
-//     await ADD.newFunction1({ a: 5, b: 15 }, { c: `stringhere` })
-//   )
-//   console.log(
-//     'Outputs NF1: ',
-//     await ADD.newFunction1({ a: 5, b: 15 }, { d: 10 })
-//   )
-//   console.log('Outputs NF2: ', await newFunction2({ a: 5, b: 15 }, { d: 10 }))
-//   console.log(
-//     'Outputs NF2: ',
-//     await newFunction2({ a: 5, b: 15 }, { c: 'custom', d: 20 })
-//   )
-
-//   console.log('special output: ', await HOF(add)({ a: 5, b: 15 }))
-// }
-
-// run()
+ruleCheck();
