3c9c7be97534c8572873e0611bef0b3881a387d5
Abdul Fourteia <raoph40@gmail.com>
2024-03-17 01:30:10 +0200

    adding some defaults for procedures

--- server/src/procedures/_config.ts
+++ server/src/procedures/_config.ts
@@ -1,3 +1,5 @@
 export const DEFAULT_MAX_RETRIES = 5;
 export const DEFAULT_PAGE_SIZE = 50;
 export const DEFAULT_PAGE_NUMBER = 0;
+export const DEFAULT_SORT_ORDER = "asc";
+export const DEFAULT_SORT_BY = "id";
